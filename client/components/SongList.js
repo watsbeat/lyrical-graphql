fccfe4386070ddee1f1acc6c61306f2910daadde
Caitlin Watson-Beaton <c.watsonbeaton@gmail.com>
2021-02-01 18:51:14 +1000

    handle lists; refetch query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
   renderSongs() {
@@ -21,24 +21,12 @@ class SongList extends Component {
     return (
       <div>
         <ul className="collection">{this.renderSongs()}</ul>
-        <Link
-          to="/songs/new"
-          className="btn-floating btn-large red right"
-        >
+        <Link to="/songs/new" className="btn-floating btn-large red right">
           <i className="material-icons">add</i>
         </Link>
       </div>
     );
   }
 }
-
-const query = gql`
-  {
-    songs {
-      id
-      title
-    }
-  }
-`;
 
 export default graphql(query)(SongList);
