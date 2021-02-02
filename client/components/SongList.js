8f7650c3ca01201702b7eef692620eba03ec6af9
Caitlin Watson-Beaton <c.watsonbeaton@gmail.com>
2021-02-02 11:14:33 +1000

    associate mutation w/ component; invoke delete mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/fetchSongs';
+import DeleteSong from '../queries/deleteSong';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       );
     });
@@ -29,4 +37,4 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+export default graphql(DeleteSong)(graphql(query)(SongList));
